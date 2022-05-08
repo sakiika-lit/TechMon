38541cef6ee9ee101ccccca09780b4c13ec6fc57
saki shishikura <saki.cec.419@gmail.com>
2022-05-08 10:03:26 +0900

    Initial Commit

---
+++ TechMon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechMon
+//
+//  Created by saki shishikura on 2022/05/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
